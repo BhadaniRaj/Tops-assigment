41a27d54b29cb7ba8b2bb5670045a3a1950b14ca
BhadaniRaj <115863801+BhadaniRaj@users.noreply.github.com>
2022-10-15 12:22:24 +0530

    module1 uploded

---
+++ basiccode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+    printf("HELLO WORlD \n NAME\t\t:\tRaj\n BRTHDATE\t:\t04/05/2004 \n AGE\t\t:\t19 \n ADDRESS\t:\t40 satyam row house near mahadev mandir mota varachha surat " );
+    
+    return 0;
+}
